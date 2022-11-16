9290b7be9aa6756ec157101cf962a0f65ae2cd8d
Izmaylova <Natalyatf@yandex.ru>
2022-11-16 21:53:29 +0500

    Домашнее задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Картошка", 19, 1.5));
+        list.add(new Product("Помидоры", 129.99, 1.2));
+        list.add(new Product("Огурцы", 69, 0.8));
+        list.add(new Product("Морковь", 40, 0.5));
+
+
+        System.out.println(list);
+
+        hasDuplicates(list);
+
+
+    }
+
+    public static <T> boolean hasDuplicates(List<T> list){
+        int count = list.size();
+        T t1,t2;
+        for(int i=0;i<count;i++){
+            t1 = list.get(i);
+            for(int j=i+1;j<count;j++){
+                t2 = list.get(j);
+                if(t2.equals(t1)){
+                    throw new RuntimeException("Этот продукт уже куплен - есть в списке!");
+                }
+            }
+        }
+        return false;
     }
 }
+
+
--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,58 @@
+import java.util.Iterator;
+import java.util.Objects;
+
 public class Product {
+
+    private String name;
+    private double price;
+    private double kg;
+
+
+    public Product(String name, double price, double kg) {
+        this.name = name;
+        this.price = price;
+        this.kg = kg;
+        if (name == null || name.isEmpty() || name.isBlank())
+            throw new RuntimeException("Заполните карточку товара!");
+        if (price <= 0 || kg <= 0)
+            throw new RuntimeException("Заполните карточку товара");
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getKg() {
+        return kg;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(product.price, price) == 0 && Double.compare(product.kg, kg) == 0 && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, kg);
+    }
+
+    @Override
+    public String toString() {
+
+        return "{ " + name  +
+                ": цена: " + price + " руб" +
+                ", вес: " + kg + " кг " +
+                '}';
+
+    }
 
 
 }
